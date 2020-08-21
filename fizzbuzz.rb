fc94f710839bc8cb8f54b1477318d382c5f84622
Kevin Glasgow <kglas001@gmail.com>
2020-08-21 01:13:21 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz ()
-  puts 15
-  puts 7
+  puts "15"
+  puts "4"
 end 
